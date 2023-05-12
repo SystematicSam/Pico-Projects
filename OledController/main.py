761651dd98e9931cb992d9a71f74f9aaecc9da35
SystematicSam <systematicsam@protonmail.com>
2023-05-12 12:17:57 +1000

    Added OledController project to repository.

---
+++ OledController/main.py
@@ -0,0 +1,28 @@
+"""
+OLED Controller
+
+This Micropython script acts as an interface for the PiicoDev SSD1306
+OLED Display Module.
+
+Main functionality is printing.  Calling print
+functions will display text on the OLED Module line by line similar to a
+terminal console.
+
+This script is most useful for debugging information when using other
+PiicoDev sensors.
+
+Author: Sam Rogers
+
+Created: 12/05/2023
+"""
+
+
+def main():
+    """
+    Main Loop
+    """
+
+
+if __name__ == '__main__':
+    main()
+

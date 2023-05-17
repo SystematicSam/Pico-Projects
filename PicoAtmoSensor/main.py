70ff22a9fc45861836fdfe906892d9efe8c491d1
SystematicSam <systematicsam@protonmail.com>
2023-05-17 17:47:22 +1000

    Added functionality to main.py in PicoAtmoSensor project to print sensor output to console display (OLED).

--- PicoAtmoSensor/main.py
+++ PicoAtmoSensor/main.py
@@ -18,19 +18,40 @@ __version__ = "0.1"
 
 
 def main():
-    while True:
-        # Print data
-        tempC, presPa, humRH = sensor.values()
-        print(str(tempC) + " °C  " + str(presPa) + " hPa  " +
-              str(humRH) + "%RH")
+    """
+    Main Loop
 
-        # Altitude demo
-        print(sensor.altitude() - zeroAlt)
+    Retrieves temperature, humidity, pressure and altitude readings
+    from sensor and prints to console display.
+    """
+    # Print startup message:
+    display.println("Starting measurements:")
+    display.refresh()
+    for i in range(4):
+        sleep_ms(100)
+        display.println("   .")
+        display.refresh()
+    display.line_pos = [0, 15, 27, 39, 51]
+    while True:
+        # Retrieve data
+        tempC, presPa, humRH = sensor.values()
+        heading = "---Atmo Sensor---"
+        temp = "TMP: " + str(tempC) + chr(176) + "C"
+        pres = "PRE: " + str(presPa / 100) + "hPa"
+        hum = "HUM: " + str(round(humRH, 2)) + "%RH"
+        alt = "ALT: " + str(round(sensor.altitude() - zero, 2)) + "m"
+
+        # Display data
+        display.data = [heading, temp, pres, hum, alt]
+        display.refresh()
+
+        # 100 millisecond intervals
         sleep_ms(100)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     sensor = PiicoDev_BME280()  # initialise the sensor
-    zeroAlt = sensor.altitude()  # take an initial altitude reading
+    zero = sensor.altitude()  # take an initial altitude reading
+    display = Console() # initalise OLED module for console printing
     main()

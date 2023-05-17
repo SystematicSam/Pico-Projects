b4f4752c3a55311a4cee67bbd0c859128e207a06
SystematicSam <systematicsam@protonmail.com>
2023-05-17 17:03:38 +1000

    Added functionality to main() in PicoAtmoSensor project.  Script now gets sensor values and prints to PC console.

--- PicoAtmoSensor/main.py
+++ PicoAtmoSensor/main.py
@@ -2,8 +2,8 @@
 PiicoDev BME280 Atmospheric Sensor Program
 
 This program reads Temperature, Pressure and Relative Humidity
-from the PiicoDev Atmospheric Sensor. An altitude reading is also
-available.
+from the PiicoDev Atmospheric Sensor and outputs to PiicoDev OLED
+Display Module. An altitude reading is also available.
 
 Author: Sam Rogers
 
@@ -17,12 +17,19 @@ __version__ = "0.1"
 
 
 def main():
-    """
-    Main Loop
-    """
-    print("Hello")
+    while True:
+        # Print data
+        tempC, presPa, humRH = sensor.values()
+        print(str(tempC) + " °C  " + str(presPa) + " hPa  " +
+              str(humRH) + "%RH")
+
+        # Altitude demo
+        print(sensor.altitude() - zeroAlt)
+        sleep_ms(100)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    sensor = PiicoDev_BME280()  # initialise the sensor
+    zeroAlt = sensor.altitude()  # take an initial altitude reading
     main()

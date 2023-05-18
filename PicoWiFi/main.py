6a6721d72b02155f624967a32e239a86a827b0ee
SystematicSam <systematicsam@protonmail.com>
2023-05-18 12:47:47 +1000

    Added description to main.py describing module.

--- PicoWiFi/main.py
+++ PicoWiFi/main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
+"""
+Pico Wi-Fi
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+This Micropython script acts as either an autonomous program or a
+project attachable script to connect a Pico W device to the internet.
 
+Main functionality is reading in a Wi-Fi configuration from config file
+and then attempting a connection based on that configuration.  Errors
+connecting are detected and appropriate messages displayed to user.
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+This script is most useful for testing a connection (autonomous) or
+establishing a connection for data transfer later (project attachable).
 
+Author: Sam Rogers
+
+Created: 18/05/2023
+"""
+__author__ = "Sam Rogers"
+__version__ = "0.1"
+
+def main():
+    pass
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()

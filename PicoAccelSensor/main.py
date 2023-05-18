521e634ed4feb346ac665f24214a1fca4b46191a
SystematicSam <systematicsam@protonmail.com>
2023-05-18 12:26:22 +1000

    Implemented functionality in main() to print linear accelerations to OLED display console.

--- PicoAccelSensor/main.py
+++ PicoAccelSensor/main.py
@@ -14,26 +14,38 @@ from PiicoDev_Unified import sleep_ms
 from console import Console
 
 __author__ = "Sam Rogers"
-__version__ = "0.1"
+__version__ = "0.2"
 
 
 def main():
     """
     Main Loop
 
-
+    Retrieves linear acceleration readings from sensor and prints to
+    console display.
     """
+    # Print startup message:
+    display.println("Starting measurements:")
+    display.refresh()
+    for i in range(4):
+        sleep_ms(100)
+        display.println("   .")
+        display.refresh()
+    # Change line positions to make first line appear as a heading
+    display.line_pos = [0, 15, 27, 39, 51]
     while True:
         x, y, z = sensor.acceleration
-        x = round(x, 2)
-        y = round(y, 2)
-        z = round(z, 2)
-        readout = "X: " + str(x) + ", Y: " + str(y) + ", Z: " + str(z)
-        print(readout)
+        display.data[0] = "-Accelerometer-"
+        display.data[1] = "X: %.2f" % x
+        display.data[2] = "Y: %.2f" % y
+        display.data[3] = "Z: %.2f" % z
+        display.refresh()
+        sleep_ms(10)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     sensor = PiicoDev_LIS3DH()
     sensor.range = 2
+    display = Console()
     main()

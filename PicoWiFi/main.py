edd23939a93a9926207bd9910a71832966027591
SystematicSam <systematicsam@protonmail.com>
2023-05-19 12:45:57 +1000

    Added connect() method to WiFi class.

--- PicoWiFi/main.py
+++ PicoWiFi/main.py
@@ -16,7 +16,10 @@ Author: Sam Rogers
 Created: 18/05/2023
 """
 import sys
+import time
+
 import ujson
+import network
 
 __author__ = "Sam Rogers"
 __version__ = "0.1"
@@ -31,9 +34,47 @@ class WiFi:
     def __init__(self, cfg: dict):
         self.ssid = cfg.get("ssid", DEFAULT_WIFI["ssid"])
         self.pwd = cfg.get("pwd", DEFAULT_WIFI["pwd"])
-
         print("SSID:\t" + self.ssid)
         print("PWD:\t" + self.pwd)
+
+    def connect(self):
+        # Setup Pico in station mode and attempt Wi-Fi connection
+        wlan = network.WLAN(network.STA_IF)
+        wlan.active(True)
+        wlan.connect(self.ssid, self.pwd)
+
+        # Wait for connection
+        timeout = 0
+        while timeout < 10:
+            """
+            # wlan.status() return values and meanings:
+                0  STAT_LINK_DOWN       -> Link is down
+                1  STAT_LINK_JOIN       -> Connecting to wifi
+                2  STAT_LINK_NOIP       -> Connected to wifi, no ip
+                3  STAT_LINK_UP         -> Connected to wifi, got ip
+                -1 STAT_LINK_FAIL       -> Connection attempted, but failed
+                -2 STAT_LINK_NONET      -> No network found with ssid
+                -3 STAT_LINK_BADAUTH    -> Bad authentication (wrong password)
+            """
+            if wlan.status() < 0 or wlan.status() >= 3:
+                break
+            timeout += 1
+            print("Waiting for connection...", timeout)
+            time.sleep(1)
+
+        # Handle connection status
+        if wlan.status() != 3:
+            # Connection failed for some reason
+            raise RuntimeError("Wi-Fi Connection Failed!\nError Code:",
+                               wlan.status())
+        else:
+            # Connection successful
+            print("Wi-Fi Connected!")
+            status = wlan.ifconfig()
+            print("  IP Address:\t", status[0])
+            print("  Subnet Mask:\t", status[1])
+            print("  Gateway:\t", status[2])
+            print("  DNS Server:\t", status[3])
 
 
 def get_config(path: str) -> dict:
@@ -44,6 +85,7 @@ def get_config(path: str) -> dict:
 
 def main(cfg: dict):
     wifi = WiFi(cfg.get("wifi"))
+    wifi.connect()
 
 
 # Press the green button in the gutter to run the script.

352fbd04f2ce0d3919427b89533dc38dc902a48a
SystematicSam <systematicsam@protonmail.com>
2023-05-21 14:40:44 +1000

    Added LedControllerUI project to repository.

---
+++ LedControllerUI/main.py
@@ -0,0 +1,16 @@
+"""
+LED Controller Interface
+"""
+import network
+import socket
+from machine import Pin
+
+__author__ = "Sam Rogers"
+__version__ = "0.1"
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()

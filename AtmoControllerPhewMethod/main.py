9cfffc67fe8d43b98fd13a0f32084a3852fccd40
SystematicSam <systematicsam@protonmail.com>
2023-05-24 14:40:31 +1000

    Added AtmoControllerPhewMethod project to repository.

---
+++ AtmoControllerPhewMethod/main.py
@@ -0,0 +1,17 @@
+"""
+PiicoDev BME280 Atmospheric Sensor Controller
+"""
+
+__author__ = "Sam Rogers"
+__version__ = "0.1"
+
+
+def main():
+    """
+    Main Loop
+    """
+    pass
+
+
+if __name__ == '__main__':
+    main()

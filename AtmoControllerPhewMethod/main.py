7adfb42ac60002a8339cbee19068e7e0b12449cf
SystematicSam <systematicsam@protonmail.com>
2023-05-24 15:18:26 +1000

    Removed connect_to_wifi() function since using Phew library.

--- AtmoControllerPhewMethod/main.py
+++ AtmoControllerPhewMethod/main.py
@@ -5,7 +5,10 @@ import network
 import socket
 import time
 import ujson
+from phew import server, connect_to_wifi
+from phew.template import render_template
 from PiicoDev_BME280 import PiicoDev_BME280
+
 
 __author__ = "Sam Rogers"
 __version__ = "0.1"
@@ -17,52 +20,6 @@ DEFAULT_WIFI = {
 }
 
 
-def connect_to_wifi(ssid, pwd):
-    """
-    Connect to an existing Wi-Fi, using the given ssid and password.
-
-    :param ssid: the ssid of the Wi-Fi network
-    :param pwd: the password to connect to the network
-    """
-    # Setup Pico in station mode and attempt Wi-Fi connection
-    print(ssid, pwd)
-    wlan = network.WLAN(network.STA_IF)
-    wlan.active(True)
-    wlan.connect(ssid, pwd)
-
-    # Wait for connection
-    timeout = 0
-    while timeout < 10:
-        """
-        # wlan.status() return values and meanings:
-            0  STAT_LINK_DOWN       -> Link is down
-            1  STAT_LINK_JOIN       -> Connecting to wifi
-            2  STAT_LINK_NOIP       -> Connected to wifi, no ip
-            3  STAT_LINK_UP         -> Connected to wifi, got ip
-            -1 STAT_LINK_FAIL       -> Connection attempted, but failed
-            -2 STAT_LINK_NONET      -> No network found with ssid
-            -3 STAT_LINK_BADAUTH    -> Bad authentication (wrong password)
-        """
-        if wlan.status() < 0 or wlan.status() >= 3:
-            break
-        timeout += 1
-        print("Waiting for connection...", timeout)
-        time.sleep(1)
-
-    # Handle connection status
-    if wlan.status() != 3:
-        # Connection failed for some reason
-        raise RuntimeError(
-            "Wi-Fi Connection Failed!\nError Code:" + str(wlan.status()))
-    else:
-        # Connection successful
-        status = wlan.ifconfig()
-        print("Wi-Fi Connected!")
-        print("  IP Address:\t", status[0])
-        print("  Subnet Mask:\t", status[1])
-        print("  Gateway:\t", status[2])
-        print("  DNS Server:\t", status[3])
-
 
 def main():
     """
@@ -72,6 +29,8 @@ def main():
         cfg = ujson.load(f)
     connect_to_wifi(cfg.get("ssid", DEFAULT_WIFI["ssid"]),
                     cfg.get("pwd", DEFAULT_WIFI["pwd"]))
+    atmo = PiicoDev_BME280()
+    zero = atmo.altitude()
 
 
 if __name__ == '__main__':
